f8fa03574f5993454fce64647487ccd953d7b044
Junaem <junh9547@gmail.com>
2021-09-18 21:37:22 +0900

    junh practice first commit

---
+++ practice/junhyong/src/argorithm_0917/b_10871.java
@@ -0,0 +1,16 @@
+package argorithm_0917;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b_10871 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int n= Integer.parseInt(st.nextToken());
+		int x= Integer.parseInt(st.nextToken());
+		
+	}
+}
