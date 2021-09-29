5fefafa1638ad420b1674a2e3f5f77230c978b7a
sungu <sungu1516@gmail.com>
2021-09-29 21:05:18 +0900

    Upload 0929

---
+++ practice/sungu/ch7/src/arraylist/StudentTest.java
@@ -0,0 +1,19 @@
+package arraylist;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student studentLee= new Student(1001, "Lee");
+		studentLee.addSubject("국어", 100);
+		studentLee.addSubject("수학", 55);
+		
+		Student studentKim = new Student(1002, "Kim");
+		studentKim.addSubject("국어", 90);
+		studentKim.addSubject("수학", 55);
+		
+		studentLee.showStudentInfo();
+		System.out.println("======================");
+		studentKim.showStudentInfo();
+	}
+
+}
